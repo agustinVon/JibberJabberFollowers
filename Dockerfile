13ed3bc3b248f8be60ee55ad8e59a893f5b3fe8d
agustinVon <agustin.von@ing.austral.edu.ar>
2022-06-20 16:25:13 -0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM gradle:6.8.0-jdk11
+COPY . /home/gradle/src
+WORKDIR /home/gradle/src
+RUN gradle build
+EXPOSE 8083
+ENTRYPOINT ["java","-jar","/home/gradle/src/build/libs/JibberJabber-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/com/agustinvon/jibberjabberfollowers/JibberjabberFollowersApplication.java
@@ -0,0 +1,13 @@
+package com.agustinvon.jibberjabberfollowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibberjabberFollowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JibberjabberFollowersApplication.class, args);
+	}
+
+}
